33043fd410f2d7700a4acaec327b0572face3799
Adastephen <adakolestephen09@gmail.com>
2024-01-31 17:03:46 +0100

    snake xenzer

---
+++ snake.js
@@ -0,0 +1,28 @@
+// call the board div in the DOM
+
+// design the grid into the board
+function createGrid() {
+    let board = document.querySelector('#board');
+
+    board.style.gridTemplateColumns = `repeat(4, 1fr)`;
+    board.style.gridTemplateRows = `repeat(4, 1fr)`;
+    board.style = 'display:flex border: 1px solid black; padding: 10px;';
+
+    // create 4 squares
+    // add event listeners to each square
+
+    for (let a = 0; a < 4; a++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', function() {
+            this.style.backgroundColor = 'blue';
+        });
+        square.addEventListener('mouseout', function() {
+            this.style.backgroundColor = 'white';
+        });
+        board.appendChild(square);
+    };
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    createGrid();
+});
